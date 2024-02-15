45e6ddb3899bff5ed632a8094d3ec1899efb4cae
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-02-15 19:54:02 +0300

    Exclude commons-logging from all configurations.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -113,14 +113,14 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springdoc-openapi.version"]}")
-    implementation("org.octopusden.octopus.octopus-external-systems-clients:bitbucket-client:${project.properties["external-systems-client.version"]}") {
-        exclude("commons-logging", "commons-logging")
-    }
-    implementation("org.octopusden.octopus.octopus-external-systems-clients:gitea-client:${project.properties["external-systems-client.version"]}") {
-        exclude("commons-logging", "commons-logging")
-    }
+    implementation("org.octopusden.octopus.octopus-external-systems-clients:bitbucket-client:${project.properties["external-systems-client.version"]}")
+    implementation("org.octopusden.octopus.octopus-external-systems-clients:gitea-client:${project.properties["external-systems-client.version"]}")
     implementation("org.gitlab4j:gitlab4j-api:${project.properties["gitlab4j-api.version"]}")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(project(":test-common"))
+}
+
+configurations.all {
+    exclude("commons-logging", "commons-logging")
 }
