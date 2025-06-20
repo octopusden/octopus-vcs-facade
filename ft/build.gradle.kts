283ef8fd7ad92a5949ae1b53c318411f028c7f54
adelline kania <78481457+adellinekania@users.noreply.github.com>
2025-06-20 14:03:23 +0700

    Switched to octopus-oc-template-gradle-plugin (#69)

--- ft/build.gradle.kts
+++ ft/build.gradle.kts
@@ -1,15 +1,14 @@
 import com.avast.gradle.dockercompose.ComposeExtension
 import java.util.Base64
-import org.octopusden.octopus.service.OcTemplateService
 
 plugins {
     id("com.avast.gradle.docker-compose")
+    id("org.octopusden.octopus.oc-template")
 }
 
 fun String.getExt() = project.ext[this] as String
 
 val commonOkdParameters = mapOf(
-    "DEPLOYMENT_PREFIX" to "vcs-facade-ft-$version".replace("[^-a-z0-9]".toRegex(), "-"),
     "ACTIVE_DEADLINE_SECONDS" to "okdActiveDeadlineSeconds".getExt(),
     "DOCKER_REGISTRY" to "dockerRegistry".getExt()
 )
@@ -26,54 +25,56 @@ fun String.getDockerHost() = "localhost:${getPort()}"
 
 fun String.getDockerExternalHost() = "$this:${getPort()}"
 
-fun String.getOkdPod() = "${commonOkdParameters["DEPLOYMENT_PREFIX"]}-$this"
+fun String.getOkdPod(): String = ocTemplate.getPod(this)
 
-fun String.getOkdHost() = "${getOkdPod()}-route-${"okdProject".getExt()}.${"okdClusterDomain".getExt()}:80"
+fun String.getOkdHost(): String = ocTemplate.getOkdHost(this)
 
 fun String.getOkdExternalHost() = "${getOkdPod()}-service:${getPort()}"
 
-val gitea = gradle.sharedServices.registerIfAbsent("giteaFtService", OcTemplateService::class.java) {
-    parameters.apply {
-        namespace = "okdProject".getExt()
-        templateFile = rootProject.layout.projectDirectory.file("okd/gitea.yaml")
-        templateParameters.putAll(commonOkdParameters)
-        templateParameters.put("GITEA_IMAGE_TAG", properties["gitea.image-tag"] as String)
-        workDirectory = layout.buildDirectory.dir("okd")
-    }
-}
+ocTemplate {
+    namespace.set("okdProject".getExt())
+    workDir.set(layout.buildDirectory.dir("okd"))
 
-val opensearch = gradle.sharedServices.registerIfAbsent("opensearchFtService", OcTemplateService::class.java) {
-    parameters.apply {
-        namespace = "okdProject".getExt()
-        templateFile = rootProject.layout.projectDirectory.file("okd/opensearch.yaml")
-        templateParameters.putAll(commonOkdParameters)
-        templateParameters.put("OPENSEARCH_IMAGE_TAG", properties["opensearch.image-tag"] as String)
-        workDirectory = layout.buildDirectory.dir("okd")
-    }
-}
+    clusterDomain.set("okdClusterDomain".getExt())
+    prefix.set("vcs-facade-ft")
 
-val bitbucket = gradle.sharedServices.registerIfAbsent("bitbucketFtService", OcTemplateService::class.java) {
-    parameters.apply {
-        namespace = "okdProject".getExt()
-        templateFile = rootProject.layout.projectDirectory.file("okd/bitbucket.yaml")
-        templateParameters.putAll(commonOkdParameters)
-        templateParameters.put("BITBUCKET_LICENSE", Base64.getEncoder().encodeToString("bitbucketLicense".getExt().toByteArray()))
-        templateParameters.put("BITBUCKET_IMAGE_TAG", properties["bitbucket.image-tag"] as String)
-        templateParameters.put("POSTGRES_IMAGE_TAG", properties["postgres.image-tag"] as String)
-        workDirectory = layout.buildDirectory.dir("okd")
+    group("giteaServices").apply {
+        enabled.set("testProfile".getExt() == "gitea")
+        service("gitea") {
+            templateFile.set(rootProject.layout.projectDirectory.file("okd/gitea.yaml"))
+            parameters.set(commonOkdParameters + mapOf("GITEA_IMAGE_TAG" to properties["gitea.image-tag"] as String))
+        }
+        service("opensearch") {
+            templateFile.set(rootProject.layout.projectDirectory.file("okd/opensearch.yaml"))
+            parameters.set(commonOkdParameters + mapOf("OPENSEARCH_IMAGE_TAG" to properties["opensearch.image-tag"] as String))
+        }
     }
-}
 
-val vcsFacade = gradle.sharedServices.registerIfAbsent("vcsFacadeFtService", OcTemplateService::class.java) {
-    parameters.apply {
-        namespace = "okdProject".getExt()
-        templateFile = rootProject.layout.projectDirectory.file("okd/vcs-facade.yaml")
-        templateParameters.putAll(commonOkdParameters)
-        templateParameters.put("VCS_FACADE_IMAGE_TAG", version as String)
-        templateParameters.put("VCS_FACADE_VCS_TYPE", "testProfile".getExt())
-        templateParameters.put("VCS_FACADE_VCS_HOST", "testProfile".getExt().getOkdExternalHost())
-        templateParameters.put("VCS_FACADE_OPENSEARCH_HOST", "opensearch".getOkdExternalHost())
-        workDirectory = layout.buildDirectory.dir("okd")
+    group("bitbucketServices").apply {
+        enabled.set("testProfile".getExt() == "bitbucket")
+        service("bitbucket") {
+            templateFile.set(rootProject.layout.projectDirectory.file("okd/bitbucket.yaml"))
+            parameters.set(commonOkdParameters + mapOf(
+                "BITBUCKET_LICENSE" to Base64.getEncoder().encodeToString("bitbucketLicense".getExt().toByteArray()),
+                "BITBUCKET_IMAGE_TAG" to properties["bitbucket.image-tag"] as String,
+                "POSTGRES_IMAGE_TAG" to properties["postgres.image-tag"] as String
+            ))
+        }
+    }
+
+    service("vcs-facade") {
+        templateFile.set(rootProject.layout.projectDirectory.file("okd/vcs-facade.yaml"))
+        parameters.set(commonOkdParameters + mapOf(
+            "VCS_FACADE_IMAGE_TAG" to version as String,
+            "VCS_FACADE_VCS_TYPE" to "testProfile".getExt(),
+            "VCS_FACADE_VCS_HOST" to "testProfile".getExt().getOkdExternalHost(),
+            "VCS_FACADE_OPENSEARCH_HOST" to "opensearch".getOkdExternalHost()
+        ))
+        if ("testProfile".getExt() == "gitea") {
+            dependsOn.set(listOf("gitea", "opensearch"))
+        } else {
+            dependsOn.set(listOf("bitbucket"))
+        }
     }
 }
 
@@ -90,7 +91,7 @@ configure<ComposeExtension> {
             "POSTGRES_IMAGE_TAG" to properties["postgres.image-tag"],
             "GITEA_IMAGE_TAG" to properties["gitea.image-tag"],
             "OPENSEARCH_IMAGE_TAG" to properties["opensearch.image-tag"],
-            "VCS_FACADE_IMAGE_TAG" to version
+            "VCS_FACADE_IMAGE_TAG" to version as String
         )
     )
 }
@@ -135,63 +136,8 @@ val ft by tasks.creating(Test::class) {
             systemProperties["test.vcs-external-host"] = "testProfile".getExt().getOkdExternalHost()
             systemProperties["test.vcs-facade-host"] = "vcs-facade".getOkdHost()
             systemProperties["test.vcs-facade-external-host"] = "vcs-facade".getOkdExternalHost()
-            when ("testProfile".getExt()) {
-                "gitea" -> {
-                    usesService(gitea)
-                    usesService(opensearch)
-                    usesService(vcsFacade)
-                    doFirst {
-                        gitea.get().create()
-                        opensearch.get().create()
-                        gitea.get().waitPodsForReady()
-                        opensearch.get().waitPodsForReady()
-                        vcsFacade.get().create()
-                        vcsFacade.get().waitPodsForReady()
-                        "okdWebConsoleUrl".getExt().let {
-                            if (it.isNotBlank()) {
-                                logger.quiet("FT gitea pod: ${it}/k8s/ns/${"okdProject".getExt()}/pods/${"gitea".getOkdPod()}")
-                                logger.quiet("FT opensearch pod: ${it}/k8s/ns/${"okdProject".getExt()}/pods/${"opensearch".getOkdPod()}")
-                                logger.quiet("FT vcs-facade pod: ${it}/k8s/ns/${"okdProject".getExt()}/pods/${"vcs-facade".getOkdPod()}")
-                            }
-                        }
-                    }
-                    doLast {
-                        gitea.get().logs("gitea".getOkdPod())
-                        opensearch.get().logs("opensearch".getOkdPod())
-                        vcsFacade.get().logs("vcs-facade".getOkdPod())
-                        vcsFacade.get().delete()
-                        gitea.get().delete()
-                        opensearch.get().delete()
-                    }
-                }
-
-                "bitbucket" -> {
-                    usesService(bitbucket)
-                    usesService(vcsFacade)
-                    doFirst {
-                        bitbucket.get().create()
-                        bitbucket.get().waitPodsForReady()
-                        vcsFacade.get().create()
-                        vcsFacade.get().waitPodsForReady()
-                        "okdWebConsoleUrl".getExt().let {
-                            if (it.isNotBlank()) {
-                                logger.quiet("FT bitbucket-db pod: ${it}/k8s/ns/${"okdProject".getExt()}/pods/${"bitbucket-db".getOkdPod()}")
-                                logger.quiet("FT bitbucket pod: ${it}/k8s/ns/${"okdProject".getExt()}/pods/${"bitbucket".getOkdPod()}")
-                                logger.quiet("FT vcs-facade pod: ${it}/k8s/ns/${"okdProject".getExt()}/pods/${"vcs-facade".getOkdPod()}")
-                            }
-                        }
-                    }
-                    doLast {
-                        bitbucket.get().logs("bitbucket-db".getOkdPod())
-                        bitbucket.get().logs("bitbucket".getOkdPod())
-                        vcsFacade.get().logs("vcs-facade".getOkdPod())
-                        vcsFacade.get().delete()
-                        bitbucket.get().delete()
-                    }
-                }
-            }
+            ocTemplate.isRequiredBy(this)
         }
-
         "docker" -> {
             systemProperties["test.vcs-host"] = "testProfile".getExt().getDockerHost()
             systemProperties["test.vcs-external-host"] = "testProfile".getExt().getDockerExternalHost()
