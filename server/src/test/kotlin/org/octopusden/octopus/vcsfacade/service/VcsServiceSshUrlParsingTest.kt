2306157c28317573b21b280ea7c7688c7534d540
Gabriella Naomi <gabriellanaomi37@gmail.com>
2026-04-14 10:46:40 +0700

    Extract sub and add test case

--- server/src/test/kotlin/org/octopusden/octopus/vcsfacade/service/VcsServiceSshUrlParsingTest.kt
+++ server/src/test/kotlin/org/octopusden/octopus/vcsfacade/service/VcsServiceSshUrlParsingTest.kt
@@ -30,30 +30,7 @@ class VcsServiceSshUrlParsingTest {
             password = null,
             healthCheck = null
         )
-        return object : VcsService(properties) {
-            override fun getRepositories(): Sequence<Repository> = TODO()
-            override fun findRepository(group: String, repository: String): Repository? = TODO()
-            override fun getBranches(group: String, repository: String): Sequence<Branch> = TODO()
-            override fun getTags(group: String, repository: String): Sequence<Tag> = TODO()
-            override fun createTag(group: String, repository: String, createTag: CreateTag): Tag = TODO()
-            override fun getTag(group: String, repository: String, name: String): Tag = TODO()
-            override fun deleteTag(group: String, repository: String, name: String) = TODO()
-            override fun getCommits(group: String, repository: String, from: HashOrRefOrDate<String, Date>?, toHashOrRef: String): Sequence<Commit> = TODO()
-            override fun getCommitsWithFiles(group: String, repository: String, from: HashOrRefOrDate<String, Date>?, toHashOrRef: String): Sequence<CommitWithFiles> = TODO()
-            override fun getBranchesCommitGraph(group: String, repository: String): Sequence<CommitWithFiles> = TODO()
-            override fun getCommit(group: String, repository: String, hashOrRef: String): Commit = TODO()
-            override fun getCommitWithFiles(group: String, repository: String, hashOrRef: String): CommitWithFiles = TODO()
-            override fun getPullRequests(group: String, repository: String): Sequence<PullRequest> = TODO()
-            override fun createPullRequest(group: String, repository: String, createPullRequest: CreatePullRequest): PullRequest = TODO()
-            override fun getPullRequest(group: String, repository: String, index: Long): PullRequest = TODO()
-            override fun findTags(group: String, repository: String, names: Set<String>): Sequence<Tag> = TODO()
-            override fun findCommits(group: String, repository: String, hashes: Set<String>): Sequence<Commit> = TODO()
-            override fun findPullRequests(group: String, repository: String, indexes: Set<Long>): Sequence<PullRequest> = TODO()
-            override fun findBranches(issueKey: String): Sequence<Branch> = TODO()
-            override fun findCommits(issueKey: String): Sequence<Commit> = TODO()
-            override fun findCommitsWithFiles(issueKey: String): Sequence<CommitWithFiles> = TODO()
-            override fun findPullRequests(issueKey: String): Sequence<PullRequest> = TODO()
-        }
+        return StubVcsService(properties)
     }
 
     private val bitbucketService = createService("ssh://git@bitbucket.example.com", VcsServiceType.BITBUCKET)
@@ -113,7 +90,39 @@ class VcsServiceSshUrlParsingTest {
     }
 
     @Test
+    fun `gitea parse SCP-style URL with colon`() {
+        val (group, repository) = giteaService.parse("ssh://git@gitea.example.com:org/repo.git")
+        assertEquals("org", group)
+        assertEquals("repo", repository)
+    }
+
+    @Test
     fun `isSupported does not match host with dots as wildcards`() {
         assertFalse(bitbucketService.isSupported("ssh://git@bitbucketXexampleYcom/project/repo.git"))
+    }
+
+    private class StubVcsService(properties: VcsProperties.Service) : VcsService(properties) {
+        override fun getRepositories(): Sequence<Repository> = TODO()
+        override fun findRepository(group: String, repository: String): Repository? = TODO()
+        override fun getBranches(group: String, repository: String): Sequence<Branch> = TODO()
+        override fun getTags(group: String, repository: String): Sequence<Tag> = TODO()
+        override fun createTag(group: String, repository: String, createTag: CreateTag): Tag = TODO()
+        override fun getTag(group: String, repository: String, name: String): Tag = TODO()
+        override fun deleteTag(group: String, repository: String, name: String) = TODO()
+        override fun getCommits(group: String, repository: String, from: HashOrRefOrDate<String, Date>?, toHashOrRef: String): Sequence<Commit> = TODO()
+        override fun getCommitsWithFiles(group: String, repository: String, from: HashOrRefOrDate<String, Date>?, toHashOrRef: String): Sequence<CommitWithFiles> = TODO()
+        override fun getBranchesCommitGraph(group: String, repository: String): Sequence<CommitWithFiles> = TODO()
+        override fun getCommit(group: String, repository: String, hashOrRef: String): Commit = TODO()
+        override fun getCommitWithFiles(group: String, repository: String, hashOrRef: String): CommitWithFiles = TODO()
+        override fun getPullRequests(group: String, repository: String): Sequence<PullRequest> = TODO()
+        override fun createPullRequest(group: String, repository: String, createPullRequest: CreatePullRequest): PullRequest = TODO()
+        override fun getPullRequest(group: String, repository: String, index: Long): PullRequest = TODO()
+        override fun findTags(group: String, repository: String, names: Set<String>): Sequence<Tag> = TODO()
+        override fun findCommits(group: String, repository: String, hashes: Set<String>): Sequence<Commit> = TODO()
+        override fun findPullRequests(group: String, repository: String, indexes: Set<Long>): Sequence<PullRequest> = TODO()
+        override fun findBranches(issueKey: String): Sequence<Branch> = TODO()
+        override fun findCommits(issueKey: String): Sequence<Commit> = TODO()
+        override fun findCommitsWithFiles(issueKey: String): Sequence<CommitWithFiles> = TODO()
+        override fun findPullRequests(issueKey: String): Sequence<PullRequest> = TODO()
     }
 }
